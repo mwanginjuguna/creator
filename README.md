4b75dc0df0832d6d07a46e27bf080e1cfdeeea5b
Kanothe <francis.kanothe@gmail.com>
2024-02-02 16:14:02 +0300

    Creator mode. Setup UI configs.

---
+++ README.md
@@ -0,0 +1,20 @@
+<p align="center"><a href="https://mwangikanothe.com" target="_blank"><img src="https://github.com/mwanginjuguna/public-image-assets/blob/main/profileImg.jpg?raw=true" width="400" alt="Mwangi Kanothe"></a></p>
+
+<p align="center">
+<a href="https://mwangikanothe.com/blog">Writer</a> |
+<a href="https://mwangikanothe.com/dev">Software Developer</a> |
+<a href="https://mwangikanothe.com/dev">ML engineer</a> |
+<a href="https://mwangikanothe.com/shop">Entrepreneur</a>
+</p>
+
+## About Me
+
+Hello,
+
+My name is Francis Mwangi Njuguna (Kanothe).
+
+I am a creator, entrepreneur, and innovator. This is my portfolio website.
+
+## License
+
+Creator is open-sourced software licensed under the [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: ['Libre Baskerville', ...defaultTheme.fontFamily.serif],
+                body: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'creator-green': '#023F3A',
+                'creator-green-light': '#075953',
+                'creator-orange': '#C13512',
+                'creator-dark': '#222222',
+                'creator-light': '#F2EEE9',
+            }
+        },
+    },
+
+    plugins: [forms],
+};
